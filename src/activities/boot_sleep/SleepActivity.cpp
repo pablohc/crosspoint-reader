ea54af46bbcec7fc84efcb334726fe13bd600da7
pablohc <pablonoviello@outlook.com>
2026-05-17 21:59:44 +0200

    feat: apply custom cross logo to boot and sleep screens

--- src/activities/boot_sleep/SleepActivity.cpp
+++ src/activities/boot_sleep/SleepActivity.cpp
@@ -14,7 +14,7 @@
 #include "Epub/converters/DirectPixelWriter.h"
 #include "activities/reader/ReaderUtils.h"
 #include "fontIds.h"
-#include "images/Logo120.h"
+#include "images/CrossLarge.h"
 
 namespace {
 constexpr uint8_t SLEEP_FACTORY_INTERNAL_PREFLASH_PASSES = 0;
@@ -207,9 +207,9 @@ void SleepActivity::renderDefaultSleepScreen() const {
   const auto pageHeight = renderer.getScreenHeight();
 
   renderer.clearScreen();
-  renderer.drawImage(Logo120, (pageWidth - 120) / 2, (pageHeight - 120) / 2, 120, 120);
+  renderer.drawImage(CrossLarge, (pageWidth - 128) / 2, (pageHeight - 128) / 2, 128, 128);
   renderer.drawCenteredText(UI_10_FONT_ID, pageHeight / 2 + 70, tr(STR_CROSSPOINT), true, EpdFontFamily::BOLD);
-  renderer.drawCenteredText(SMALL_FONT_ID, pageHeight / 2 + 95, tr(STR_SLEEPING));
+  renderer.drawCenteredText(SMALL_FONT_ID, pageHeight / 2 + 95, tr(STR_SLEEPING), true, EpdFontFamily::BOLD);
 
   // Make sleep screen dark unless light is selected in settings
   if (SETTINGS.sleepScreen != CrossPointSettings::SLEEP_SCREEN_MODE::LIGHT) {
